650a413bae116554696619a8c10b9a9db1199ebb
Dariusz Okoński <darek200180@gmail.com>
2024-05-05 12:56:11 +0200

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1 className='text-3xl font-bold underline'>Hello World</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
