1673a608b8180e56355eb895f9d643429616dbc2
Dariusz Okoński <darek200180@gmail.com>
2024-05-06 19:10:51 +0200

    add icons for swapping themes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,15 @@ import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 import NavLinks from './NavLinks';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [theme, setTheme] = useState(false);
+
+  const handleTheme = () => {
+    setTheme(!theme);
+  };
+
   return (
     <nav className='bg-base-200'>
       <div className='navbar align-element'>
@@ -34,7 +41,12 @@ const Navbar = () => {
           </ul>
         </div>
         <div className='navbar-end'>
-          {/* THEME */}
+          {/* THEME SETUP*/}
+          <label className='swap swap-rotate'>
+            <input type='checkbox' onChange={handleTheme} />
+            <BsSunFill className='swap-on h-4 w-4' />
+            <BsMoonFill className='swap-off h-4 w-4' />
+          </label>
           {/* CART LINK */}
           <NavLink to='/cart' className='btn btn-ghost btn-circle btn-md ml-4'>
             <div className='indicator'>
