8f5fea7189d7a44405ace466bcb4198fbf1f512f
Dariusz Okoński <darek200180@gmail.com>
2024-05-09 18:49:19 +0200

    add new componentes for products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,23 @@
+import React from 'react';
+import { Filters, PaginationContainer, ProductsContainer } from '../components';
+import { customFetch } from '../utils';
+const url = '/products';
+
+export const loader = async ({ request }) => {
+  const response = await customFetch(url);
+  const products = response.data.data;
+  const meta = response.data.meta;
+  return { products, meta };
+};
+
 const Products = () => {
-  return <div className='text-4xl'>Products</div>;
+  return (
+    <React.Fragment>
+      <Filters />
+      <ProductsContainer />
+      <PaginationContainer />
+    </React.Fragment>
+  );
 };
 
 export default Products;
